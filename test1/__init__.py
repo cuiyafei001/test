85372ade782ab30b5d0c48e82747961212880ec6
cuiyafei001 <cuiyafei001@163.com>
2017-11-13 22:37:14 +0800

    python 2 语法基础练习

---
+++ test1/__init__.py
@@ -0,0 +1,84 @@
+# _*_ coding: UTF-8 _*_
+# module.py
+#   def main():
+#     print "we are in %s"%__name__
+#   if __name__ == '__main__':
+#     main()
+
+# name = raw_input('please enter your name')
+# print 'hello',name
+
+# print absolute value of an integer:
+# a = 100
+# if a >= 0:
+#     print a
+# else:
+#     print -a
+
+# print '100 + 200 = ',100 + 200
+
+# print 'I\'m,ok'
+# print 'I\'m hello \npython'
+# print '\\\n\\'
+# print '\tpython'
+# print r'\\\t\\'
+# print '''line1
+# line2
+# line3'''
+# age = 20
+# if age >= 18:
+#     print 'adult'
+# else:
+#     print 'teenager'
+# a = 123
+# print (a)
+# a = 'ABC'
+# b = a
+# a = 'xyz'
+# print (b)
+# print u'中'
+# -*- coding:gbk -*-
+# arr1 = [1,2,5,6,7,8,]
+# arr2 = [0,5,4,1,0,5,2,5,5,1,3]
+# tel = ""
+# for i in arr2:
+#     tel = tel + str(arr1[i])
+#
+# print '联系方式：'+ tel
+#
+#
+# arr = [8,2,1,5,7,6]
+# index = [2,0,4,1,2,0,3,0,0,1,5]
+# tel = 0
+# for i in  index:
+#     s = arr[i]
+#     tel = tel * 10+ s
+# print(tel)
+
+# student = ['Aleen','Bob','cici']
+# for i in student:
+#     s = student[1]
+#
+#     print s
+"""
+if True:
+    print "True"
+    print "Answer"
+else:
+    print "Answer"
+print "False
+"""
+a = 1
+b = 2
+#执行输出
+print a
+print b
+
+print '-------'
+#不换行输出
+print a,
+print b,
+
+print a,b
+
+a = 2
