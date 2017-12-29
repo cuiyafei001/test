aba4f81c0d619f9588da0db52e1e942f41151789
cuiyafei001 <cuiyafei001@163.com>
2017-12-29 22:54:15 +0800

    selenium 定位table

---
+++ test2/test_table.py
@@ -0,0 +1,12 @@
+# _*_ coding: UTF-8 _*_
+from selenium import webdriver
+import time
+
+driver = webdriver.Firefox()
+url = "file:///C:/Users/123/Desktop/table.HTML"
+driver.get(url)
+
+t = driver.find_element_by_xpath(".//*[@id='myTable']/tbody/tr[2]/td[3]")
+print t.text
+
+driver.quit()
