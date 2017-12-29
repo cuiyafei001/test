114da43e0c9b81041c57b765ba1043423c1b4af2
cuiyafei001 <cuiyafei001@163.com>
2017-12-29 22:50:25 +0800

    int

---
+++ test2/__init__.py
@@ -0,0 +1,15 @@
+# _*_ coding: UTF-8 _*_
+from selenium import webdriver
+
+# driver = webdriver.Chrome()
+# url = "https://www.baidu.com/"
+# driver.get(url)
+
+
+# x = 1
+#
+# def fun1():
+#     print 'modulea',x
+#
+# def fun2():
+#     print 'modulea',x*2
